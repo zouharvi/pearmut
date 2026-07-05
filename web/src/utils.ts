43ff5ed50e624459b534629b73f1886dfc64bc00
David Dale <daledavidd@gmail.com>
2026-07-05 16:18:32 +0200

    add XSTS+R+P example and instructions

--- web/src/utils.ts
+++ web/src/utils.ts
@@ -576,7 +576,7 @@ export type SliderConfig = {
 
 // Shared protocol info type
 export type ProtocolInfo = {
-    protocol: "DA" | "cESA" | "ESA" | "MQM",
+    protocol: "DA" | "cESA" | "ESA" | "MQM" | "XSTSRP",
     item_i: number,
     sliders?: SliderConfig[],  // Optional custom slider configurations
     instructions?: string,
