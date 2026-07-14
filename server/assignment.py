c6721da0cc60f5299b0f2aac379b86ff5d457670
Vilém Zouhar <vilem.zouhar@gmail.com>
2026-07-14 18:33:30 +0200

    sample according to 1/sqrt(rank)

--- server/assignment.py
+++ server/assignment.py
@@ -624,7 +624,7 @@ def get_next_item_dynamic(
         }
         model_weights_dict = {
             # 1/(rank + 1) to give higher weight to better performing models
-            model: 1 / (rank + 1)
+            model: 1 / (rank + 1)**0.5
             for rank, model in enumerate(
                 sorted(model_avg_scores.keys(), key=lambda x: model_avg_scores[x], reverse=True)
             )
